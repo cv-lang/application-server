c981bf30d56bfd4a6fcef00b623c4c9016500216
MikeDev505 <mikedev505@gmail.com>
2020-07-25 15:12:46 +0200

    układanie app server

---
+++ Cvl.ApplicationServer/Cvl.ApplicationServer.Standard/ProcessesEngine/Model/ProcessDescription.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cvl.ApplicationServer.ProcessesEngine.Model
+{
+    public class ProcessDescription
+    {
+    }
+}
---
+++ Cvl.ApplicationServer/Cvl.ApplicationServer.Standard/Server/Interfaces/IApplicatnioServer.cs
@@ -0,0 +1,14 @@
+﻿using Cvl.ApplicationServer.Jobs.Model;
+using Cvl.ApplicationServer.ProcessesEngine.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cvl.ApplicationServer.Server.Interfaces
+{
+    public interface IApplicatnioServer
+    {
+        List<JobDescription> GetJobs();
+        List<ProcessTypeDescription> GetProcessTypes();
+    }
+}
