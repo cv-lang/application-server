76ca615971144a3a7c49cee54a61bff8168d7a26
MikeDev505 <mikedev505@gmail.com>
2021-04-18 15:18:19 +0200

    log

---
+++ Cvl.ApplicationServer/Cvl.ApplicationServer.Server/Database/LogContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cvl.ApplicationServer.Server.Database
+{
+    public class LogContext : DbContext
+    {
+    }
+}
